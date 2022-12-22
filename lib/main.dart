46b310e93d8685b49d8415a0cdeb9dfc5f5a471a
amr <amr@amrs-MBP.home>
2022-12-22 11:24:45 -0800

    Test Token from Android Studio

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.purple,
+          primarySwatch: Colors.red,
           scaffoldBackgroundColor: Colors.grey[200],
           fontFamily: 'Reggae One',
         ),
